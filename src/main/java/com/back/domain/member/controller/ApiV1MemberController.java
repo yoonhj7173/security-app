a817424e773b53542971e742d1f6733a99e4f123
yoonhj7173 <yoonhj7173@gmail.com>
2026-03-31 11:20:21 +0900

    cookies for authorization

--- src/main/java/com/back/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/back/domain/member/controller/ApiV1MemberController.java
@@ -6,6 +6,7 @@ import com.back.domain.member.service.MemberService;
 import com.back.global.exception.ServiceException;
 import com.back.global.rq.Rq;
 import com.back.global.rsData.RsData;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class ApiV1MemberController {
     private final MemberService memberService;
     private final Rq rq;
+    private final HttpServletResponse response;;
 
     record MemberJoinReqBody(
             String username,
@@ -64,6 +66,8 @@ public class ApiV1MemberController {
         if(!actor.getPassword().equals(reqBody.password)){
             throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
         }
+
+        rq.addCookie("apiKey", actor.getApiKey());
 
         return new RsData(
                 "%s님 환영합니다.".formatted(actor.getName()),
--- src/main/java/com/back/global/rq/Rq.java
+++ src/main/java/com/back/global/rq/Rq.java
@@ -3,9 +3,13 @@ package com.back.global.rq;
 import com.back.domain.member.entity.Member;
 import com.back.domain.member.service.MemberService;
 import com.back.global.exception.ServiceException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 @RequiredArgsConstructor
@@ -15,24 +19,49 @@ public class Rq {
     // client가 보낸 모든 정보가 담겨있음. Spring이 자동으로 해줌
     private final HttpServletRequest request;
     private final MemberService memberService;
+    private final HttpServletResponse response;
+
+    public void addCookie(String name, String value) {
+
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setDomain("localhost");
+
+        response.addCookie(
+                cookie
+        );
+    }
 
     public Member getActor() {
 
         String authorizationHeader = request.getHeader("Authorization");
 
-        if (authorizationHeader == null) {
-            throw new ServiceException("401-1", "인증 정보가 헤더에 존재하지 않습니다.");
+        String apiKey = null;
+
+        // 헤더 방식 vs 쿠키 방식
+        if (authorizationHeader != null) {
+            // throw new ServiceException("401-1", "인증 정보가 헤더에 존재하지 않습니다.");
+            if (!authorizationHeader.startsWith("Bearer ")) {
+                throw new ServiceException("401-2", "잘못된 형식의 인증 데이터입니다.");
+            }
+            apiKey = authorizationHeader.replace("Bearer ", "");
+        } else {
+            apiKey = request.getCookies() == null ? ""
+                    : Arrays.stream(request.getCookies())
+                    .filter(cookie -> cookie.getName().equals("apiKey"))
+                    .map(cookie -> cookie.getValue())
+                    .findFirst()
+                    .orElse("");
+
         }
 
-        if (!authorizationHeader.startsWith("Bearer ")) {
-            throw new ServiceException("401-2", "잘못된 형식의 인증 데이터입니다.");
+        if (apiKey.isBlank()) {
+            throw new ServiceException("401-3", "인증 정보가 존재하지 않습니다.");
         }
-
-        String apiKey = authorizationHeader.replace("Bearer ", "");
 
         return memberService.findByApiKey(apiKey).orElseThrow(
                 () -> new ServiceException("401-1", "유효하지 않은 API 키입니다.")
         );
     }
-
 }
